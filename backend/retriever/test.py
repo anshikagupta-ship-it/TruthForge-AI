a66f79c34e9be3aa14994f284fa305d31b2863f6
Kartikeya Srivastav <kartikeya251@gmail.com>
2026-07-26 17:41:35 +0530

    fix

--- backend/retriever/test.py
+++ backend/retriever/test.py
@@ -1,1 +1,14 @@
 print("Test works",flush=True)
+print("1", flush=True)
+
+import torch
+
+print("2", flush=True)
+
+import transformers
+
+print("3", flush=True)
+
+import sentence_transformers
+
+print("4", flush=True)
